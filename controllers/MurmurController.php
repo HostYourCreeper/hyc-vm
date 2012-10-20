bf9267010cf6571357617937601a2471857f94c4
Emilien Kenler <hello@emilienkenler.com>
2012-10-20 20:57:48 +0200

    Murmur
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- controllers/MurmurController.php
+++ controllers/MurmurController.php
@@ -6,11 +6,11 @@ class MurmurController {
   {
     $action = (isset($request->url_elements[2]) && !empty($request->url_elements[2])) ? $request->url_elements[2] : false;
     if(!$action)
-      return $request->error(404,'What would you do with minecraft?');
+      return $request->error(404,'What would you do with murmur?');
     if(!in_array($action, array('start','stop','restart')))
       return $request->error(404,'You can\'t do that.');
     
-    $result = shell_exec("/etc/init.d/minecraft ".$action);
+    $result = shell_exec("/etc/init.d/murmur ".$action);
     if(!$result)
       return $request->error(500,'An error occured while executing command.');
     else
